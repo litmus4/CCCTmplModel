7ca69b28020f3932dd77ad8fe01305d4000805fe
litmus4 <scrudlds@163.com>
2018-03-01 17:03:18 +0800

    TextTableCenter

---
+++ Build/assets/Script/DataTables/TextTableCenter.js
@@ -0,0 +1,83 @@
+var JsonTableParser = require("JsonTableParser");
+var LoadController = require("LoadController");
+
+var TextTableCenter = {
+    textParser : null,
+    captionParser : null,
+    captionTagMap : null,
+    storyTextParser : null,
+
+    Init : function(sLanguage, bUseCaptionTags)
+    {
+        var sFolder = "DataTables/TextTable/" + sLanguage + "/";
+
+        this.textParser = new JsonTableParser();
+        this.textParser.Load(sFolder + "Text", LoadController.GetCheckFunc(), true);
+
+        this.captionParser = new JsonTableParser();
+        this.captionParser.Load(sFolder + "Caption", LoadController.GetCheckFunc(), true);
+        if (bUseCaptionTags)
+        {
+            for (var sID in this.captionParser.data)
+                this.captionTagMap[this.captionParser.GetRow(sID).Tag] = sID;
+        }
+
+        this.storyTextParser = new JsonTableParser();
+        this.storyTextParser.Load(sFolder + "StoryText", LoadController.GetCheckFunc(), true);
+    },
+
+    Release : function()
+    {
+        this.textParser = null;
+        this.captionParser = null;
+        this.captionTagMap = null;
+        this.storyTextParser = null;
+    },
+
+    GetTextRow : function(sID)
+    {
+        return this.textParser.GetRow(sID);
+    },
+
+    GetText : function(sID)
+    {
+        var row = this.textParser.GetRow(sID);
+        return row ? row.Text : "";
+    },
+
+    GetCaptionRow : function(sID)
+    {
+        return this.captionParser.GetRow(sID);
+    },
+
+    GetCaption : function(sID)
+    {
+        var row = this.captionParser.GetRow(sID);
+        return row ? row.Text : "";
+    },
+
+    GetCaptionByTag : function(sTag)
+    {
+        var sID = this.captionTagMap[sTag];
+        if (sID)
+        {
+            var row = this.captionParser.GetRow(sID);
+            if (row)
+                return row.Text;
+        }
+        return "";
+    },
+
+    GetStoryTextRow : function(sID)
+    {
+        return this.storyTextParser.GetRow(sID);
+    },
+
+    GetStoryText : function(sID)
+    {
+        var row = this.storyTextParser.GetRow(sID);
+        return row ? row.Text : "";
+    }
+};
+
+module.exports = TextTableCenter;
