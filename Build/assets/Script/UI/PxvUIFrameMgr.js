c39f30caf6944789df21ef3f2f294fe924a8c515
litmus4 <scrudlds@163.com>
2018-07-19 11:18:16 +0800

    nodeFrame窗口初始位置

--- Build/assets/Script/UI/PxvUIFrameMgr.js
+++ Build/assets/Script/UI/PxvUIFrameMgr.js
@@ -1,11 +1,14 @@
 var PxvUIFrameMgr = {
     nodeLayer : null,
+    vLayerPosi : null,
     nodeFrameMap : {},
     stackFrames : [],
 
     Init : function()
     {
         this.nodeLayer = cc.find("Canvas/UILayer");
+        //UILayer的尺寸要与Canvas一致，而下边的(0.5,0.5)表示UI编辑场景的Canvas使用默认锚点
+        this.vLayerPosi = cc.p(this.nodeLayer.width * 0.5, this.nodeLayer.height * 0.5);
     },
 
     LoadFromPrefab : function(sFile, binder, fnCallback, bFrame)
@@ -20,6 +23,14 @@ var PxvUIFrameMgr = {
             var nodePrefab = cc.instantiate(prefab);
             if (nodePrefab)
             {
+                var nodeFrameInfo = this.nodeFrameMap[sFile];
+                if (nodeFrameInfo)
+                {
+                    var fPfNegaX = -(nodePrefab.width * nodePrefab.anchorX);
+                    var fPfNegaY = -(nodePrefab.height * nodePrefab.anchorY);
+                    var pos = cc.pAdd(this.vLayerPosi, nodePrefab.position);
+                    nodeFrameInfo.pos = cc.pAdd(pos, cc.p(fPfNegaX, fPfNegaY));
+                }
                 nodePrefab.setAnchorPoint(0, 0);
                 nodePrefab.position = cc.p(0, 0);
             }
@@ -32,12 +43,12 @@ var PxvUIFrameMgr = {
 
             if (fnCallback)
                 fnCallback(sFile, nodePrefab);
-        });
+        }.bind(this));
 
         if (binder && bFrame)
         {
             this.nodeFrameMap[sFile] = {
-                frame : binder, node : null, bFilled : false
+                frame : binder, node : null, pos : cc.p(0, 0), bFilled : false
             };
         }
     },
@@ -51,7 +62,10 @@ var PxvUIFrameMgr = {
         if (nodeFrameInfo)
         {
             if (!nodeFrameInfo.node)
+            {
                 nodeFrameInfo.node = node;
+                node.position = nodeFrameInfo.pos;
+            }
             
             node.setAnchorPoint(0, 0);
             node.setContentSize(nodePrefab.getContentSize());
@@ -74,12 +88,15 @@ var PxvUIFrameMgr = {
             for (var sFile in this.nodeFrameMap)
             {
                 var curNodeFrameInfo = this.nodeFrameMap[sFile];
-                if (curNodeFrameInfo.frame === frame)
+                if (curNodeFrameInfo.frame === frame && !curNodeFrameInfo.node)
                 {
                     curNodeFrameInfo.node = node;
+                    node.position = curNodeFrameInfo.pos;
                     break;
                 }
             }
         }
     }
 };
+
+module.exports = PxvUIFrameMgr;
