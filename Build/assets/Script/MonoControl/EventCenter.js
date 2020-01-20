c3550b241f54fd4f9e33983eef8074365a277274
litmus4 <scrudlds@163.com>
2020-01-20 18:58:58 +0800

    EventCenter FLAGJK

---
+++ Build/assets/Script/MonoControl/EventCenter.js
@@ -0,0 +1,83 @@
+var EventCenter = {
+    EKey: {
+        //
+    },
+
+    delegateMap: {},
+
+    Register: function(eKey, fnCallback, obj)
+    {
+        if (!fnCallback) return;
+
+        var delegate = this.delegateMap[eKey];
+        if (!delegate)
+        {
+            delegate = {
+                listMap: {},
+                Do: function(param)
+                {
+                    for (var syKey in this.listMap)
+                    {
+                        var listener = this.listMap[syKey];
+                        listener.objs.forEach(function(curObj, i){
+                            if (curObj === "function")
+                                listener.fn(param);
+                            else
+                                listener.fn.call(curObj, param);
+                        });
+                    }
+                }
+            };
+            this.delegateMap[eKey] = delegate;
+        }
+
+        var syKey = Symbol.for(fnCallback.toString());
+        var listener = delegate.listMap[syKey];
+        if (!listener)
+        {
+            listener = {
+                fn: fnCallback,
+                objs: [obj || "function"]
+            };
+            delegate.listMap[syKey] = listener;
+        }
+        else
+            listener.objs.push(obj || "function");
+    },
+
+    UnRegister: function(eKey, fnCallback, obj)
+    {
+        if (!fnCallback) return;
+
+        var delegate = this.delegateMap[eKey];
+        if (!delegate) return;
+
+        var syKey = Symbol.for(fnCallback.toString());
+        var listener = delegate.listMap[syKey];
+        if (listener)
+        {
+            for (var i = listener.objs.length - 1; i >= 0; --i)
+            {
+                var curObj = listener.objs[i];
+                if (curObj === (obj || "function"))
+                    listener.objs.splice(i, 1);
+            }
+            if (listener.objs.length === 0)
+                delete delegate.listMap[syKey];
+        }
+    },
+
+    SendEvent: function(eKey, param)
+    {
+        var delegate = this.delegateMap[eKey];
+        if (delegate)
+            delegate.Do(param);
+    },
+
+    Clear: function(eKey)
+    {
+        //FLAGJK
+    }
+};
+
+module.exports = EventCenter;
